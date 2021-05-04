f26e7799807cf52ee9fac8d5762b364b55be5207
Cicero Gasparini Brunelli <brunelli.cg@gmail.com>
2021-05-04 13:05:11 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,15 @@
+
+function Footer() {
+    return (
+        <div>
+            <footer className="footer mt-auto py-3 bg-dark">
+                <div className="container">
+                    <p className="text-light"><small><strong>Semana Spring React</strong><br />
+                     Evento promovido pela escola DevSuperior</small></p>
+                </div>
+            </footer>
+        </div>
+    )
+}
+
+export default Footer
