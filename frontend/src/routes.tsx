a1bcd4fd0cfde293a9447293e6e68e14936b0e3e
Cicero Gasparini Brunelli <brunelli.cg@gmail.com>
2021-05-07 16:45:49 -0300

    First request

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -6,13 +6,15 @@ import Footer from 'components/Footer';
 
 export function Routes() {
     return (
-        <BrowserRouter>
-            <Navbar />
-            <Switch>
-                <Route path='/' exact component={Home} />
-                <Route path='/dashboard' component={Dashboard} />
-            </Switch>
-            <Footer />
-        </BrowserRouter>
+        <>
+            <BrowserRouter>
+                <Navbar />
+                <Switch>
+                    <Route path='/' exact component={Home} />
+                    <Route path='/dashboard' component={Dashboard} />
+                </Switch>
+                <Footer />
+            </BrowserRouter>
+        </>
     )
 }
